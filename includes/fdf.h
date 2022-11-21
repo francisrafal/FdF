6bf214d6f8643a5016842eac7ba2be93afd00852
Francis Rafal <francis.rafal@gmail.com>
2022-11-21 17:05:40 +0100

    feat: Print Rectangles

--- includes/fdf.h
+++ includes/fdf.h
@@ -6,7 +6,7 @@
 /*   By: frafal <marvin@42.fr>                      +#+  +:+       +#+        */
 /*                                                +#+#+#+#+#+   +#+           */
 /*   Created: 2022/11/21 12:50:01 by frafal            #+#    #+#             */
-/*   Updated: 2022/11/21 14:45:33 by frafal           ###   ########.fr       */
+/*   Updated: 2022/11/21 17:05:03 by frafal           ###   ########.fr       */
 /*                                                                            */
 /* ************************************************************************** */
 
@@ -18,13 +18,22 @@
 # include <math.h>
 # include <X11/keysym.h>
 
-# define WIN_WIDTH 1280
-# define WIN_HEIGHT 720
+# define WIN_WIDTH 1920
+# define WIN_HEIGHT 1080
 # define GREEN 0x0000FF00
 
 typedef struct	s_data {
 	void		*mlx_ptr;
 	void		*win_ptr;
 }				t_data;
+
+typedef struct	s_rect
+{
+	int			x;
+	int			y;
+	int			width;
+	int			height;
+	int			color;
+}				t_rect;
 
 #endif
--- src/main.c
+++ src/main.c
@@ -6,36 +6,67 @@
 /*   By: frafal <marvin@42.fr>                      +#+  +:+       +#+        */
 /*                                                +#+#+#+#+#+   +#+           */
 /*   Created: 2022/11/14 14:17:05 by frafal            #+#    #+#             */
-/*   Updated: 2022/11/21 14:48:18 by frafal           ###   ########.fr       */
+/*   Updated: 2022/11/21 17:04:41 by frafal           ###   ########.fr       */
 /*                                                                            */
 /* ************************************************************************** */
 
 #include "fdf.h"
 
-void	draw_circle(void *mlx_ptr, void *win_ptr, int color)
+void	draw_circle(t_data *data, int color)
 {
 	double_t	phi;
 
 	phi = 0;
 	while (phi < 2 * M_PI)
 	{
-		int x = round(100*cos(phi) + 640);
-		int y = round(100*sin(phi) + 360);
-		mlx_pixel_put(mlx_ptr, win_ptr, x, y, color);
-		phi += 0.01;
+		int x = round(WIN_WIDTH / 10 * cos(phi) + WIN_WIDTH / 2);
+		int y = round(WIN_WIDTH / 10 * sin(phi) + WIN_HEIGHT / 2);
+		mlx_pixel_put(data->mlx_ptr, data->win_ptr, x, y, color);
+		phi += 0.001;
 	}
 }
 
-void	draw_vline(void *mlx_ptr, void *win_ptr, int x, int color)
+void	draw_vline(t_data *data, int x, int color)
 {
 	int i;
 
 	i = 0;
-	while (i < 720)
+	while (i < WIN_HEIGHT)
 	{
-		mlx_pixel_put(mlx_ptr, win_ptr, x, i, color);
+		mlx_pixel_put(data->mlx_ptr, data->win_ptr, x, i, color);
 		i++;
 	}
+}
+
+void	draw_rect(t_data *data, t_rect rect)
+{
+	int	i;
+	int	j;
+
+	i = rect.y;
+	while (i < rect.y + rect.height)
+	{
+		j = rect.x;
+		while (j < rect.x + rect.width)
+		{
+			if (i == rect.y || i == rect.y + rect.height - 1 || j == rect.x || j == rect.x + rect.width - 1)
+				mlx_pixel_put(data->mlx_ptr, data->win_ptr, j, i, rect.color);
+			j++;
+		}
+		i++;
+	}
+}
+
+int	loop_hook(t_data *data)
+{
+	if (data->win_ptr != NULL)
+	{
+		draw_vline(data, WIN_WIDTH / 2, GREEN);
+		draw_circle(data, GREEN);
+		draw_rect(data, (t_rect){0, WIN_HEIGHT / 4, WIN_WIDTH / 10, WIN_HEIGHT / 2, GREEN});
+		draw_rect(data, (t_rect){WIN_WIDTH / 10 * 9, WIN_HEIGHT / 4, WIN_WIDTH / 10, WIN_HEIGHT / 2, GREEN});
+	}
+	return (0);
 }
 
 int	key_hook(int keysym, t_data *data)
@@ -43,12 +74,8 @@ int	key_hook(int keysym, t_data *data)
 	if (keysym == XK_Escape)
 	{
 		mlx_destroy_window(data->mlx_ptr, data->win_ptr);
-		mlx_destroy_display(data->mlx_ptr);
-		free(data->mlx_ptr);
-		free(data);
-		exit(0);
+		data->win_ptr = NULL;
 	}
-	//ft_putnbr_fd(keycode, 1);
 	return (0);
 }
 
@@ -77,9 +104,11 @@ int	main(int argc, char **argv)
 		ft_putstr_fd("Failed to create a new window\n", 2);
 		return (-1);
 	}
-	draw_vline(data->mlx_ptr, data->win_ptr, 640, GREEN);
-	draw_circle(data->mlx_ptr, data->win_ptr, GREEN);
+	mlx_loop_hook(data->mlx_ptr, loop_hook, data);
 	mlx_key_hook(data->win_ptr, key_hook, data);
 	mlx_loop(data->mlx_ptr);
+	mlx_destroy_display(data->mlx_ptr);
+	free(data->mlx_ptr);
+	free(data);
 	return (0);
 }
