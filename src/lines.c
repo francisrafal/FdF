eb0ba82ff4275d4e248e9d85f235b3f7a27e1fa9
Francis Rafal <francis.rafal@gmail.com>
2022-11-25 14:42:13 +0100

    fix: Norm Errors

--- src/lines.c
+++ src/lines.c
@@ -22,20 +22,17 @@ int	draw_line_low(t_img *img, t_pt start, t_pt end)
 	delta = vec_sub(end, start);
 	yi = 1;
 	if (delta.y < 0)
-	{
 		yi = -1;
-		delta.y = -delta.y;
-	}
+	delta.y = fabsf(delta.y);
 	err = 2 * delta.y - delta.x;
 	cur = start;
 	while (cur.x < end.x)
 	{
 		img_pix_put(img, cur);
 		if (err > 0)
-		{
 			cur.y += yi;
+		if (err > 0)
 			err = err + (2 * (delta.y - delta.x));
-		}
 		else
 			err = err + 2 * delta.y;
 		cur.color = get_color(cur, start, end, delta);
@@ -54,20 +51,17 @@ int	draw_line_high(t_img *img, t_pt start, t_pt end)
 	delta = vec_sub(end, start);
 	xi = 1;
 	if (delta.x < 0)
-	{
 		xi = -1;
-		delta.x = -delta.x;
-	}
+	delta.x = fabsf(delta.x);
 	err = 2 * delta.x - delta.y;
 	cur = start;
 	while (cur.y < end.y)
 	{
 		img_pix_put(img, cur);
 		if (err > 0)
-		{
 			cur.x += xi;
+		if (err > 0)
 			err = err + (2 * (delta.x - delta.y));
-		}
 		else
 			err = err + 2 * delta.x;
 		cur.color = get_color(cur, start, end, delta);
